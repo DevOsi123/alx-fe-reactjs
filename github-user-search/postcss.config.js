56df6337eb65d72f1b4e72ce46614e07f6d9e1c8
DevOsi123 <gatelogistics@consultant.com>
2025-11-23 13:07:45 -0700

    new

--- github-user-search/postcss.config.js
+++ github-user-search/postcss.config.js
@@ -1,9 +1,10 @@
-import tailwindcss from '@tailwindcss/postcss';
+// postcss.config.js
+import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default {
   plugins: [
     tailwindcss(),
-    autoprefixer()
-  ]
+    autoprefixer(),
+  ],
 };
--- github-user-search/src/main.jsx
+++ github-user-search/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.jsx"; // make sure it has .jsx
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
