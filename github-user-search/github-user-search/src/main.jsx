9f49cf547114bd6de3574db5b5d5c05227e7f68c
DevOsi123 <gatelogistics@consultant.com>
2025-11-23 13:23:52 -0700

    new

---
+++ github-user-search/github-user-search/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx"; // make sure it has .jsx
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
