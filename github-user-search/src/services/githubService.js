566a36ad73a3bb0e6a7696ddaa7c2759af0fa1ae
DevOsi123 <gatelogistics@consultant.com>
2025-11-23 10:43:31 -0700

    new

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,34 @@
 // src/services/githubService.js
 import axios from "axios";
 
-export const fetchUserData = (username) => {
-  return axios.get(`https://api.github.com/users/${username}`)
-    .then(response => response.data);
+/**
+ * Fetch users from GitHub using advanced search.
+ * @param {Object} params
+ * @param {string} params.username - GitHub username
+ * @param {string} params.location - User location
+ * @param {number|string} params.minRepos - Minimum number of repos
+ * @returns {Promise<{items: Array}>} - Array of user objects
+ */
+export const fetchAdvancedUsers = async ({ username, location, minRepos }) => {
+  // Build GitHub search query
+  let query = username ? `${username} in:login` : "";
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>=${minRepos}`;
+
+  // GitHub Search API endpoint
+  const url = `https://api.github.com/search/users?q=${encodeURIComponent(
+    query
+  )}&per_page=10`; // limit results for demo
+
+  const response = await axios.get(url);
+
+  // Search API returns limited info, fetch full user data for each user
+  const detailedUsers = await Promise.all(
+    response.data.items.map(async (user) => {
+      const userDetails = await axios.get(user.url);
+      return userDetails.data;
+    })
+  );
+
+  return { items: detailedUsers };
 };
