611b6a123ddcb4e42e17cdd9f7fb4d5d7de455bf
DevOsi123 <gatelogistics@consultant.com>
2025-12-14 07:55:16 -0700

    new

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import PostsComponent from './PostsComponent.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a QueryClient instance
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
