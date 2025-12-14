1561dd50fcb526b01fae12071412110c7542211c
DevOsi123 <gatelogistics@consultant.com>
2025-12-14 08:58:51 -0700

    new

--- react-todo/src/__tests__/TodoList.test.jsx
+++ react-todo/src/__tests__/TodoList.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-env jest */
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -15,20 +16,16 @@ describe('TodoList Component', () => {
     render(<TodoList />);
     const input = screen.getByPlaceholderText('Add a new todo');
     const button = screen.getByText('Add');
-
     await userEvent.type(input, 'New Todo');
     fireEvent.click(button);
-
     expect(screen.getByText('New Todo')).toBeInTheDocument();
   });
 
   test('toggles todo completion', () => {
     render(<TodoList />);
     const todo = screen.getByText('Learn React');
-
     fireEvent.click(todo);
     expect(todo).toHaveStyle('text-decoration: line-through');
-
     fireEvent.click(todo);
     expect(todo).toHaveStyle('text-decoration: none');
   });
@@ -37,7 +34,6 @@ describe('TodoList Component', () => {
     render(<TodoList />);
     const todo = screen.getByText('Learn React');
     const deleteButton = screen.getAllByText('Delete')[0];
-
     fireEvent.click(deleteButton);
     expect(todo).not.toBeInTheDocument();
   });
