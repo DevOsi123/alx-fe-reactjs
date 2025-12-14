d0d3c4628d6387941b20362483a9e7efcea498dc
DevOsi123 <gatelogistics@consultant.com>
2025-12-14 08:25:44 -0700

    new

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth.jsx'; // ← Use the hook
 
-const ProtectedRoute = ({ isAuth, children }) => {
+const ProtectedRoute = ({ children }) => {
+  const { isAuth } = useAuth();
+
   if (!isAuth) {
     return <Navigate to="/login" replace />;
   }
+
   return children;
 };
 
